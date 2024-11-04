8a8b16380a4569539a0c2cb023a7bd712a75f24e
Aaron Raymund Paguirigan <paguiriganaaronraymund@gmail.com>
2024-11-04 21:19:08 +0800

    initial commit

---
+++ colorPref.js
@@ -0,0 +1,10 @@
+function Color(){
+    let favoriteColors  = [];
+
+    for (let i = 0; i < 3; i++) {
+        let color = prompt("Enter a favorite color:");
+        favoriteColors.push(color);
+
+    }
+    alert("Your favorite colors are: " + favoriteColors.join);    
+}
